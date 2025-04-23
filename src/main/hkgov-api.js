0f6e7ae5a77296aaef3e10fb3e67ccca4fb5e684
AngeCI <145793112+AngeCI@users.noreply.github.com>
2025-04-23 21:52:01 +0800

    Hong Kong government API support.

---
+++ src/main/hkgov-api.js
@@ -0,0 +1,108 @@
+"use strict";
+
+L.HKGov = {
+  urlBase: "https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz",
+  basemap: {
+    basemap: {
+      path: "/basemap",
+      maxZoom: 20
+    },
+    imagery: {
+      path: "/imagery",
+      maxZoom: 19
+    }
+  },
+  label: {
+    en: {
+      path: "/label/hk/en",
+      maxZoom: 20
+    },
+    tc: {
+      path: "/label/hk/tc",
+      maxZoom: 20
+    },
+    sc: {
+      path: "/label/hk/sc",
+      maxZoom: 20
+    }
+  },
+  crs: {
+    wgs84: "WGS84"
+  }
+};
+
+L.TileLayer.HKGov = L.TileLayer.extend({
+  initialize: function(base, target = "", options) {
+    let metaData = L.HKGov;
+
+    // Assign defualt values if no second parameter is provided
+    if (target == "") {
+      if (base == "basemap") {
+        target = "basemap";
+      }
+      if (base == "label") {
+        target = "tc";
+      }
+    }
+
+    if (!(base in metaData) || !(target in metaData[base])) {
+      if (!(base in metaData)) {
+        throw `Unknown basemap type "${base}"`;
+      }
+
+      if ((base in metaData) && !(target in metaData[base])) {
+        if (base == "basemap") {
+          throw `Unknown target type "${target}"`;
+        }
+        if (base == "label") {
+          throw `Unsupported label language "${target}"`;
+        }
+      }
+    }
+
+    let tileTarget = metaData[base][target];
+    let url = `${metaData.urlBase}${tileTarget.path}/${metaData.crs.wgs84}/{z}/{x}/{y}.png`;
+    let defaultOpts = {
+      maxZoom: tileTarget.maxZoom,
+    };
+    L.setOptions(this, defaultOpts);
+    L.TileLayer.prototype.initialize.call(this, url, options);
+  }
+});
+
+L.LayerGroup.HKGov = L.LayerGroup.extend({
+  initialize: function(base, label = "tc", options) {
+    let metaData = L.HKGov;
+
+    if (!(basemap in metaData.basemap)) {
+      throw `Unknown basemap type "${basemap}"`;
+    }
+    if (!(label in metaData.label)) {
+      throw `Unsupported label language "${label}"`;
+    }
+
+    this._basemapLayer = L.tileLayer.hkGov("basemap", basemap);
+    this._labelLayer = L.tileLayer.hkGov("label", label);
+
+    let myLayers = [this._basemapLayer, this._labelLayer];
+    L.LayerGroup.prototype.initialize.call(this, myLayers, options);
+  },
+  language: function(language) {
+    let metaData = L.HKGov;
+    if (!(language in metaData.label)) {
+      throw `Unsupported label language "${language}"`;
+    }
+
+    this.removeLayer(this._labelLayer);
+    this._labelLayer = L.tileLayer.hkGov("label", language);
+    this.addLayer(this._labelLayer);
+  }
+});
+
+L.tileLayer.hkGov = function(type, options) {
+  return new L.TileLayer.HKGov(type, options);
+};
+
+L.layerGroup.hkGov = function(type, options) {
+  return new L.LayerGroup.HKGov(type, options);
+};
--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,24 @@
 "use strict";
 
+import * from "./hkgov-api.js";
+
 // Base map source definitions
 const baseMaps = {
   "OpenStreetMap": L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }),
+  "CARTO": L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", {
+    maxZoom: 30,
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attribution">CARTO</a>'
+  }),
+  "HK Lands Dept Topo (No label)": L.tileLayer.hongKong("basemap", "basemap", {
+    maxZoom: 20,
+    attribution: '© <a href="https://api.portal.hkmapservice.gov.hk/disclaimer">Lands Department <img src="https://api.hkmapservice.gov.hk/mapapi/landsdlogo.jpg" width="25" height="25" /></a>'
+  }),
+  "HK Lands Dept Aerial (No label)": L.tileLayer.hongKong("basemap", "imagery", {
+    maxZoom: 20,
+    attribution: '© <a href="https://api.portal.hkmapservice.gov.hk/disclaimer">Lands Department <img src="https://api.hkmapservice.gov.hk/mapapi/landsdlogo.jpg" width="25" height="25" /></a>'
   })
 };
 
@@ -19,7 +33,25 @@ const map = L.map("map", {
 });
 L.control.scale().addTo(map);
 const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-map.on("click", (ev) => {
-  L.marker(ev.latlng).bindPopup(`WGS84 coords: ${ev.latlng.toString()}`).addTo(map);
+const ZoomViewer = L.Control.extend({
+  onAdd() {
+    const container = L.DomUtil.create("div");
+    container.style.width = "200px";
+    container.style.background = "rgba(255,255,255,0.5)";
+    container.style.textAlign = "left";
+    map.on("zoomstart zoom zoomend", (ev) => {
+      container.innerHTML = `Zoom level: ${map.getZoom()}`;
+    });
+    return container;
+  }
 });
+new ZoomViewer().addTo(map);
+
+let clickMarker;
+map.on("click", (ev) => {
+  if (clickMarker)
+    clickMarker.removeFrom(map);
+  clickMarker = L.marker(ev.latlng).bindPopup(`WGS84 coords: ${ev.latlng.toString()}`).addTo(map);
+});
+
+self.map = map;
