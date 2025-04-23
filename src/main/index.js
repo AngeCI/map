ac3b1586bb300011db6e666b7a25d3d76691021f
AngeCI <145793112+AngeCI@users.noreply.github.com>
2025-04-23 08:45:59 +0000

    Preparation for the map viewer.

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.info(`Welcome to Lightingale Hyacinth!`);
+// Base map source definitions
+const baseMaps = {
+  "OpenStreetMap": L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  })
+};
+
+const overlayMaps = {};
+
+// Map initialization
+const map = L.map("map", {
+  attributionControl: true,
+  layers: [baseMaps.OpenStreetMap],
+  center: [22.35, 114.16],
+  zoom: 11
+});
+L.control.scale().addTo(map);
+const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+map.on("click", (ev) => {
+  L.marker(ev.latlng).bindPopup(`WGS84 coords: ${ev.latlng.toString()}`).addTo(map);
+});
